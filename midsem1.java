e432e33993ee48d963c4fd426908342915b32f22
Parth Sudan <parth7k4@gmail.com>
2023-10-15 23:59:10 +0530

    midsem Q1: generics

---
+++ midsem1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Collections{
+    public static <T extends Comparable<T>> T max(Collection<T> xs){
+        Iterator<T> xi = xs.iterator();
+        T m= xi.next();
+        while(xi.hasNext()){
+            T temp = xi.next();
+            if(m.compareTo(temp)<0){
+                m= temp;
+            }
+        }
+        return m;
+    }
+}
+public class midsem1{
+    public static void main(String args[]){
+        ArrayList<Integer> a1 = new ArrayList<>();
+        a1.add(1);
+        a1.add(2);
+        a1.add(0);
+        a1.add(69);
+        a1.add(5);
+        a1.add(420);
+        a1.add(98);
+        Integer ans = Collections.max(a1); 
+        System.out.println("The max element in the collection is "+ ans.toString());   
+    }
+}
